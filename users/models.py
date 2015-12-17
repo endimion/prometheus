338c50b8d2696113b0e2418ced68811548d8ed5e
endimion <triantafyllou.ni@gmail.com>
2015-12-17 14:10:40 +0200

    start on diferenating users

--- users/models.py
+++ users/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+USER_TYPE = ( ('admin', 'Administrator'), ('civil', 'Civilian'))
+
+
 
 class User(models.Model):
     user_name = models.CharField(max_length=20)
     user_password = models.CharField(max_length=20)
     date_created = models.DateTimeField('date created')
+    user_type = models.CharField(max_length=5, choices=USER_TYPE)
+
 
     def __str__(self):
         return self.user_name
